650c50cf62e35c10f39ea6bf7a443a662e1321af
Joe-noh <goflb.jh@gmail.com>
2015-06-20 11:02:34 +0900

    入力フォームを設置
    HTMLをイジっただけなので、まだ発言できない

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,14 @@
 defmodule Shout.PageViewTest do
   use Shout.ConnCase, async: true
+
+  import Phoenix.View
+
+  test "render index page" do
+    rendered = render_to_string(Shout.PageView, "index.html", [])
+
+    # 適当。ちゃんとやるならFloki使おう
+    assert rendered =~ "word"
+    assert rendered =~ "send"
+    assert rendered =~ "log"
+  end
 end
